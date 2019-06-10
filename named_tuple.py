f4ce0c43848c5ecf426a1261654d5498f8aff52c
Sandeep Nagendra <sandeep.ee.nagendra@gmail.com>
2019-06-10 20:43:50 +0530

    Updated code

--- named_tuple.py
+++ named_tuple.py
@@ -9,12 +9,17 @@ employee = collections.namedtuple("Employee", ["Name", "Age", "DOB"])
 #Adding Values
 emp = employee("Johnny Depp", "35", "02011983")
 
-
 #initializing iterable
 ex_list = ["Abraham", "24", "20091994"]
 
 #initializing dict
 ex_dict = {"Name" : "Moti", "Age" : 29, "DOB" : "16091987"}
+
+#Below 3print statements print Same output
+print(emp.Name) #Access by key
+print(emp[0]) #Access by index
+print(getattr(emp, "Name")) #Access via getattr
+
 
 #using _make() to return namedtuple()
 print ("The namedtuple instance using iterable is : ")
@@ -28,3 +33,7 @@ print(emp._asdict())
 #using ** operator to return namedtuple from dictionary
 print("The namedtuple instance from dict is :")
 print(employee(**ex_dict))
+
+
+#print fields of the named tuple
+print(emp._fields)
