a0aab74c27be97f3070fc2a6cc4471a9513a0274
snagendr <sandeep.nagendra@nokia.com>
2019-12-31 12:09:01 +0200

    Learnt multiprocessing in python.
    Change-Id: I5b1be2c69dba21a25f4be879f9c2480182f4fa48

---
+++ multiprocessing/README.md
@@ -0,0 +1,5 @@
+Multiprocessing module helps to paralley executes tasks.
+
+For Ex:
+Processing multiple photos at the same time.
+Instead of processing one image at a time, using concurrent.futures.ProcessPoolExecutor functionality we can actually parallize processing.
---
+++ multiprocessing/multi3.py
@@ -0,0 +1,23 @@
+import time
+import multiprocessing
+
+start = time.perf_counter()
+
+def do_something():
+    print("sleeping 1 second...")
+    time.sleep(1)
+    print("Done sleeping..")
+
+processes = []
+for _ in range(10):
+    p = multiprocessing.Process(target=do_something)
+    p.start()
+    processes.append(p)
+
+for process in processes:
+    process.join()
+
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
